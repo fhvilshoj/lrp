140477964a647da941d1129b1e27b86d6688d7b7
Frederik Hvilshøj <fhvilshoj@gmail.com>
2017-10-03 14:27:01 +0200

    Fixed test example for a simple convolution test such that it passes our on board example.

--- lrp/convolutional_lrp.py
+++ lrp/convolutional_lrp.py
@@ -49,21 +49,38 @@ def convolutional(tensor, R):
     image_patches = tf.reshape(image_patches,
                                [batch, output_height, output_width, filter_height, filter_width, input_channels])
 
+    image_patches = tf.Print(image_patches, [image_patches], message="\n\nimage_patches\n", summarize=1000)
+
     # Multiply each patch by each filter to get the z_ijk's in a tensor zs
     zs = tf.multiply(tf.expand_dims(filters, 0), tf.expand_dims(image_patches, -1))
 
+    slice = zs[0,0,0,:,:,:,:]
+    zs = tf.Print(zs, [zs], message="\n\nzs\n", summarize=1000)
+    zs = tf.Print(zs, [slice], message="\n\nslice\n", summarize=1000)
+
     # Replace the negative elements with zeroes to only have the positive z's left (i.e. z_ijk^+)
     zp = lrp_util.replace_negatives_with_zeros(zs)
+    zp = tf.Print(zp, [zp], message="\n\nzp\n", summarize=1000)
 
     # Sum over the rows of z to get the z_jk's in a tensor zp_sum
     zp_sum = tf.reduce_sum(zp, [3, 4, 5], keep_dims=True)
+    zp_sum = tf.Print(zp_sum, [zp_sum], message="\n\nzp_sum\n", summarize=1000)
 
     # Reshape the relevance
+    zp_sum = tf.Print(zp_sum, [R.shape], message="\n\nR shape before reshape\n", summarize=1000)
     upper_layer_relevance = tf.reshape(R, [batch, input_height, input_width, 1, 1, 1, output_channels])
+    upper_layer_relevance = tf.Print(upper_layer_relevance, [upper_layer_relevance.shape], message="\n\nR shape after reshape\n", summarize=1000)
 
     # Find the contribution of each feature in the input to the activations,
     # i.e. the ratio between the z_ijk's and the z_jk's
-    R_new = tf.reduce_sum((zp / zp_sum) * upper_layer_relevance, 6)
+    division = (zp / zp_sum)
+    division = tf.Print(division, [division], message="\n\nDivision\n", summarize=1000)
+
+    relevance = division * upper_layer_relevance
+    relevance = tf.Print(relevance, [relevance], message="\n\nRelevance\n", summarize=1000)
+
+    summing = tf.reduce_sum(relevance, 6)
+    R_new = tf.Print(summing, [summing], message="\n\nR_new_after_reduce_sum_6\n", summarize=1000)
 
     # Reshape the relevance tensor, so each patch becomes a vector
     R_new = tf.reshape(R_new, [batch, output_height, output_width, filter_height * filter_width * input_channels])
@@ -72,13 +89,14 @@ def convolutional(tensor, R):
     R_new = lrp_util.patches_to_images(R_new, batch, input_height, input_width, input_channels, output_height,
                                        output_width, filter_height, filter_width, strides[1], strides[2], padding)
 
-    R_new = tf.Print(R_new, [conv_input], message="\n\nconv_input\n", summarize=20)
-    R_new = tf.Print(R_new, [filters], message="\n\nfilters\n", summarize=20)
-    R_new = tf.Print(R_new, [convolutiontensor], message="\n\nctensor\n", summarize=20)
-    R_new = tf.Print(R_new, [zs], message="\n\nzs\n", summarize=20)
-    R_new = tf.Print(R_new, [zs], message="\n\nzs\n", summarize=20)
-    R_new = tf.Print(R_new, [zp], message="\n\nzp\n", summarize=20)
-    R_new = tf.Print(R_new, [zp], message="\n\nzp_sum\n", summarize=20)
+    #
+    # R_new = tf.Print(R_new, [conv_input], message="\n\nconv_input\n", summarize=20)
+    # R_new = tf.Print(R_new, [filters], message="\n\nfilters\n", summarize=20)
+    # R_new = tf.Print(R_new, [convolutiontensor], message="\n\nctensor\n", summarize=20)
+    # R_new = tf.Print(R_new, [zs], message="\n\nzs\n", summarize=20)
+    # R_new = tf.Print(R_new, [zp], message="\n\nzp\n", summarize=20)
+    # R_new = tf.Print(R_new, [zp_sum], message="\n\nzp_sum\n", summarize=20)
+    # R_new = tf.Print(R_new, [relevance], message="\n\nrelevance\n", summarize=1000)
 
     # Recursively find the relevance of the next layer in the network
     return lrp._lrp(lrp_util.find_path_towards_input(convolutiontensor), R_new)
--- test/test_convolution_lrp.py
+++ test/test_convolution_lrp.py
@@ -18,22 +18,14 @@ class ConvolutionLRPTest(unittest.TestCase):
 
             # Create the convolutional layer
             # Set the weights
-
-            weights = tf.constant([[[[2, 2], [1, 2]], [[1, 2], [-1, 0]]],
-                                   [[[1, 1], [0, 1]], [[0, 0], [1, -1]]]]
-                                  , dtype=tf.float32)
-            # weights = tf.constant([[[[2, 1],
-            #                          [1, -1]],
-            #
-            #                         [[2, 2],
-            #                          [2, 0]]],
-            #
-            #                        [[[1, 0],
-            #                          [0, 1]],
-            #
-            #                         [[1, 0],
-            #                          [1, -1]]]
-            #                        ], dtype=tf.float32)
+            weights = tf.constant([[[[2., 1, 1],
+                                     [1., 1, 0]],
+                                    [[2., 2, 1],
+                                     [1., 1, 1]]],
+                                   [[[1., -1, 1],
+                                     [0., 1, 0]],
+                                    [[2., 0, 0],
+                                     [0., -1, 1]]]], dtype=tf.float32)
 
             # Perform the convolution
             activation = tf.nn.conv2d(inp, weights, [1, 1, 1, 1], "SAME")
@@ -42,7 +34,10 @@ class ConvolutionLRPTest(unittest.TestCase):
             pred = activation
 
             # Calculate the relevance scores using lrp
-            R_mock = tf.constant([[[[3, 4], [1, 1]], [[2, 3], [1, 0]]]], dtype=tf.float32)
+            R_mock = tf.constant([[[[3., 2., 1],
+                                    [4., 3., 2]],
+                                   [[1., 1., 3],
+                                    [1., 0., 4]]]], dtype=tf.float32)
             expl = lrp._lrp(pred, R_mock)
 
             # Run a tensorflow session to evaluate the graph
@@ -52,13 +47,19 @@ class ConvolutionLRPTest(unittest.TestCase):
 
                 # Run the operations of interest and feed an input to the network
                 prediction, explanation = sess.run([pred, expl],
-                                                   feed_dict={inp: [[[[1, 0], [2, -1]], [[-1, 2], [3, 0]]]]})
+                                                   feed_dict={inp: [[[[1., 0.],
+                                                                      [-1., 2.]],
+                                                                     [[2., -1.],
+                                                                      [3., 0.]]]]
+                                                              })
                 # prediction, explanation = sess.run([pred, expl], feed_dict={inp: [[[[1, -1], [2, 3]],
-                #                                                                                    [[0, 2], [-1, 0]]]
-                #                                                                                   ]})
+                #                                                                    [[0, 2], [-1, 0]]]
+                #                                                                   ]})
 
                 # Check if the predictions has the right shape
-                self.assertEqual(prediction.shape, (1, 2, 2, 2),
+                print(prediction.shape)
+                print(R_mock.shape)
+                self.assertEqual(prediction.shape, (1, 2, 2, 3),
                                  msg="Should be able to do a convolutional forward pass")
 
                 # Check if the explanation has the right shape
@@ -67,6 +68,8 @@ class ConvolutionLRPTest(unittest.TestCase):
                 # Check if the relevance scores are correct (the correct values are found by calculating the example by hand)
                 print(explanation[0])
                 self.assertTrue(
-                    np.allclose(explanation[0], [[[1.25, 0], [6 + 1 / 28, 4 + 9 / 14]],
-                                                 [[0, 3 + 1 / 14], [0, 0]]], rtol=1e-03, atol=1e-01),
+                    np.allclose(explanation[0], [[[[1.37, 0],
+                                                   [0, 6.83]],
+                                                  [[2.75, 0],
+                                                   [14.05, 0]]]], rtol=1e-01, atol=1e-01),
                     msg="Should be a good convolutional explanation")

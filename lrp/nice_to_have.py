5334ccbc443397ad05126672330ae2611ede4493
Frederik Hvilshøj <fhvilshoj@gmail.com>
2017-10-04 15:11:42 +0200

    Small fix for nice to have

--- lrp/nice_to_have.py
+++ lrp/nice_to_have.py
@@ -4,8 +4,10 @@ import tensorflow as tf
 def brackets(arr):
     res = "["
     if len(arr) > 1:
-        for _ in range(arr[0]):
+        for i in range(arr[0]):
             res += brackets(arr[1:])
+            if i < arr[0] - 1:
+                res += ","
     else:
         for _ in range(arr[0]-1):
             res += " , "

957a4a33d6b40d98e079e4767a26e092161df54b
Frederik Hvilshøj <fhvilshoj@gmail.com>
2017-10-11 18:10:32 +0200

    Added comments to LSTM test.

--- test/test_lstm_5_units_1_stacked.py
+++ test/test_lstm_5_units_1_stacked.py
@@ -3,28 +3,42 @@ import tensorflow as tf
 import numpy as np
 import unittest
 
+
 class LSTM5Units1StackedLRPTest(unittest.TestCase):
     def runTest(self):
-        with tf.Graph().as_default() as g:
+        with tf.Graph().as_default():
             lstm_units = 5
+
+            # Make static input
             np_input = np.reshape(np.arange(-10, 14), (1, 8, 3))
             inp = tf.constant(np_input, dtype=tf.float32)
 
+            # Set seed to ensure equal reaults reach time in test
             tf.set_random_seed(1337)
-            with tf.variable_scope('the_lstm'):
-                stacked_lstm = tf.contrib.rnn.LSTMCell(lstm_units,
-                                                       forget_bias=0.)  # initializer=tf.constant_initializer(1.)
-                stacked_lstm = tf.contrib.rnn.MultiRNNCell([stacked_lstm] * 1)
-                lstm_output, _ = tf.nn.dynamic_rnn(stacked_lstm, inp, dtype=tf.float32)
+
+            # Create lstm cell
+            stacked_lstm = tf.contrib.rnn.LSTMCell(lstm_units, forget_bias=0.)
+            # Put it into Multi RNN Cell
+            stacked_lstm = tf.contrib.rnn.MultiRNNCell([stacked_lstm] * 1)
+            # Let dynamic rnn setup the control flow (making while loops and stuff)
+            lstm_output, _ = tf.nn.dynamic_rnn(stacked_lstm, inp, dtype=tf.float32)
+
+            # Fakt the relevance.
             R = tf.ones_like(tf.slice(lstm_output, [0, -1, 0], [1, 1, lstm_units]))
 
+            # Get the explanation from the LRP framework.
             # TODO The output of the lstm is not quite right.
             R = lrp._lrp(inp, lstm_output, R)
 
             with tf.Session() as s:
+                # Initialize variables
                 s.run(tf.global_variables_initializer())
+
+                # Calculate relevance
                 relevances = s.run(R)
 
+                # Expected result calculated in
+                # https://docs.google.com/spreadsheets/d/1_bmSEBSWVOkpdlZYEUckgrnUtxhEfnR84LZy1cU5fIw/edit?usp=sharing
                 expected_result = np.array([[[0.04114789, 0.07254411, 0.13169597],
                                              [0.17604621, 0.1747233, 0.19547687],
                                              [0.37701777, 0.42817616, 0.29883289],
@@ -34,5 +48,7 @@ class LSTM5Units1StackedLRPTest(unittest.TestCase):
                                              [-0.12844698, -1.49989772, 1.67834044],
                                              [-3.57829499, 13.87882996, -6.72725677]]])
 
+                # Check for shape and actual result
                 self.assertTrue(relevances.shape == expected_result.shape, "Shapes not equal")
-                self.assertTrue(np.allclose(relevances, expected_result, rtol=1e-03, atol=1e-03), "NO LONGER EQUAL RELEVANCES")
+                self.assertTrue(np.allclose(relevances, expected_result, rtol=1e-03, atol=1e-03),
+                                "NO LONGER EQUAL RELEVANCES")

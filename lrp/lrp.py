e87c1d9f3ebf2f2d62c4fe40540528b32296579d
Frederik Hvilshøj <fhvilshoj@gmail.com>
2017-10-11 13:47:17 +0200

    Transformed router to use dictionary and no recursion instead of deep recursions.

--- lrp/lrp.py
+++ lrp/lrp.py
@@ -8,43 +8,52 @@ from lrp.nonlinearities_lrp import nonlinearities
 from lrp.shaping_lrp import shaping
 from lrp.lstm_lrp import lstm
 
+router = {
+    'MatMul': linear,
+    'Conv2D': convolutional,
+    'MaxPool': max_pooling,
+    'ExpandDims': shaping,
+    'Squeeze': shaping,
+    'Reshape': shaping,
+    'Relu': nonlinearities,
+    'Sigmoid': nonlinearities,
+    'Tanh': nonlinearities
+}
+# Ignores:
+# Transpose,
 
-def _lrp(tensor, R):
+
+def _lrp_routing(path, R):
     # Find the operation that created the tensor
-    operation = tensor.op
 
-    if not operation.inputs:
-        return R
+    while path:
+        print(path)
+        operation_type = path[0].type
+        if operation_type in ['Add', 'BiasAdd']:
+            # Check which operation a given addition is associated with
+            # Note that it cannot be lstm because lstm has its own scope
+            operation_type = lrp_util.addition_associated_with(path[0].outputs[0])
 
-    operation_type = operation.type
-
-    # Check if the operation is a matrix multiplication or an addition, which means that the current layer is a linear layer
-    if operation_type in ['Add', 'BiasAdd']:
-        operation_type = lrp_util.addition_associated_with(tensor)
-
-    # Route responsibility to appropriate handler
-    if operation_type in ['MatMul']:
-        return linear(tensor, R)
-    elif operation_type in ['Conv2D']:
-        return convolutional(tensor, R)
-    elif operation_type in ['ExpandDims', 'Squeeze', 'Reshape']:
-        return shaping(tensor, R)
-    elif operation_type in ['MaxPool']:
-        return max_pooling(tensor, R)
-    elif operation_type in ['Relu', 'Sigmoid', 'Tanh']:
-        return nonlinearities(tensor, R)
-    # elif 'lstm' in operation.type:
-    #     return lstm(tensor, R)
-    else:
-        return _lrp(lrp_util.find_path_towards_input(tensor), R)
+        if operation_type in router:
+            # Route responsibility to appropriate function
+            path, R = router[operation_type](path, R)
+        else:
+            path = path[1:]
+    return R
 
 
-def lrp(prediction):
-    """
-    lrp main function
-    :param prediction: Expecting a tensor containing a single prediction [h1]
-    :return: Tensor of network input size containing a relevance score for each feature of the input
-    """
+def _lrp(input, output, R):
+    path = lrp_util.get_operations_between_input_and_output(input, output)
 
     # Recurse on each layer with the relevance for the last layer set to the activation of the last layer (following the LRP convention)
-    return _lrp(prediction, prediction)
+    return _lrp_routing(path, R)
+
+
+def lrp(input, output):
+    """
+    lrp main function
+    :param input: Expecting a tensor containing a single input causing the output
+    :param output: Expecting a tensor containing a single prediction [h1]
+    :return: Tensor of input size containing a relevance score for each feature of the input
+    """
+    return _lrp(input, output, output)

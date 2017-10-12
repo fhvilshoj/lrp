1cb842837a3764ed133aa390878565012b293c93
Frederik Hvilshøj <fhvilshoj@gmail.com>
2017-10-12 08:39:55 +0200

    Few optimizations of path usage\n Added a lot of documentation.

--- lrp/lstm_lrp.py
+++ lrp/lstm_lrp.py
@@ -1,26 +1,36 @@
 import tensorflow as tf
 
 
-def _linear_lrp_helper(relevance_to_distribute, input, weights, output):
-    activations = tf.multiply(tf.transpose(input), weights)
-    fractions = tf.divide(activations, output)
-    relevances = tf.matmul(relevance_to_distribute, tf.transpose(fractions))
-    return relevances
-
-
 def _t_geq_one(t, *_):
     return tf.greater_equal(t, 1)
 
 
-def _find_operation_from_ts(ts, operation):
-    for op in ts:
+def _find_operation_from_path(path, operation):
+    """
+    This procedure takes a sub path of the output
+    to input path and searches through it until it
+    finds the operation type specified.
+    :param path: a sub path from output to input
+    :param operation: operation type (string)
+    :return: returns first occurence in path of operation
+    specified
+    """
+    for op in path:
         if op.type == operation:
             return op
     return None
 
 
-def _search_operation_path(operation, path):
-    o = operation
+def _walk_full_graph_by_path(start, path):
+    """
+    This procedure starts in operation start and
+    looks for input operations of the type given
+    in the path argument iteratively.
+    :param start: the operation in the graph to start from
+    :param path: an array of operation types (strings) to follow one at a time
+    :return: the operation at the end of the path
+    """
+    o = start
     for p in path:
         for i in o.inputs:
             if i.op.type == p:
@@ -30,33 +40,57 @@ def _search_operation_path(operation, path):
 
 
 def _handle_while_context(while_context, R, input):
-    # Find kernel, bias and additional forget bias (potentially h0 and s0)
-    bias_add_op = _find_operation_from_ts(while_context, 'BiasAdd')
-    kernel_ref = _search_operation_path(bias_add_op, ['MatMul', 'Enter', 'Identity', 'VariableV2'])
-    bias_ref = _search_operation_path(bias_add_op, ['Enter', 'Identity', 'VariableV2'])
+    """
+    Finds weights and bias and does forward pass inclusive recordings
+    of activations
+    :param while_context: sub path of the output to input path.
+    :param R: Relevance for upper layer
+    :param input: the tensor which is input to the while context
+    :return: The relevances for the lower level (of same shape as input)
+    """
 
+    # Find kernel, bias and additional forget bias
+    bias_add_op = _find_operation_from_path(while_context, 'BiasAdd')
+    kernel_ref = _walk_full_graph_by_path(bias_add_op, ['MatMul', 'Enter', 'Identity', 'VariableV2'])
+    bias_ref = _walk_full_graph_by_path(bias_add_op, ['Enter', 'Identity', 'VariableV2'])
+
+    # Both kernel and bias are references to actual objects
     kernel_ref = kernel_ref.outputs[0]
     bias_ref = bias_ref.outputs[0]
 
-    before_output_gate = _find_operation_from_ts(while_context, 'Tanh')
-    add_forget_bias_op = _search_operation_path(before_output_gate, ['Add', 'Mul', 'Sigmoid', 'Add'])
+    # Finding extra bias associated with the forget gate
+    before_output_gate = _find_operation_from_path(while_context, 'Tanh')
+    add_forget_bias_op = _walk_full_graph_by_path(before_output_gate, ['Add', 'Mul', 'Sigmoid', 'Add'])
     forget_bias = tf.identity(add_forget_bias_op.inputs[1])
 
-    state_size = bias_ref.get_shape().as_list()[0] // 4
+    # Find the number of LSTM units by dividing 4 bias entries
+    # to each unit (input, gate, forget and output bias)
+    lstm_units = bias_ref.get_shape().as_list()[0] // 4
+
+    # Find the length of the sequence
     sequence_length = input.get_shape().as_list()[1]
 
-    weights_gate_gate = tf.slice(kernel_ref, [0, state_size], [kernel_ref.get_shape().as_list()[0], state_size])
-    bias_gate_gate = tf.slice(bias_ref, [state_size], [state_size])
+    # Slicing the weights and biases used for calculating gate gate
+    # output (Ug and Wg). The kernel matrix has the following regions:
+    # ---------------------
+    # | Ui | Ug | Uf | Uo |
+    # ---------------------
+    # | Wi | Wg | Wf | Wo |
+    # ---------------------
+    # where Us are used to weight input and Ws are used for h^(t-1).
+    # Similar goes for bias.
+    weights_gate_gate = tf.slice(kernel_ref, [0, lstm_units], [kernel_ref.get_shape().as_list()[0], lstm_units])
+    bias_gate_gate = tf.slice(bias_ref, [lstm_units], [lstm_units])
 
-    # Do forward pass to collect gates, cell states and hidden states
-
-
+    # Prepare for forward pass by constructing Tensor Arrays for input,
+    # hidden states, state cells, input gate, gate gate, and forget_gate
+    # Nothing fanzy. Just empty TAs with 0's in the first entry.
     input_a = tf.TensorArray(tf.float32, size=sequence_length, clear_after_read=False)
     input_a = input_a.split(input[0], [1] * sequence_length)
 
     def get_ta_with_0_at_idx_zero(clear_after_read=True):
         ta = tf.TensorArray(tf.float32, size=sequence_length + 1, clear_after_read=clear_after_read)
-        return ta.write(0, tf.constant(0., shape=(1, state_size)))
+        return ta.write(0, tf.constant(0., shape=(1, lstm_units)))
 
     hidden_states = get_ta_with_0_at_idx_zero(False)
     state_cells = get_ta_with_0_at_idx_zero(False)
@@ -64,74 +98,106 @@ def _handle_while_context(while_context, R, input):
     gate_gate = get_ta_with_0_at_idx_zero()
     forget_gate = get_ta_with_0_at_idx_zero()
 
+    # While body for recalculating forward pass of lstm
+    # in order to record the outputs of the different
+    # states and gates.
     def _body(t, hs, sc, ig, gg, fg):
+        # Read state input from the previous time step
         i = input_a.read(t - 1)
         h = hs.read(t - 1)
         s = sc.read(t - 1)
 
+        # Concatenate input and previous hidden state to
+        # be able to multiply with kernel.
         tm = tf.concat([i, h], 1)
 
+        # Multiply tm with kernel and add bias.
         before_activation_functions = tf.nn.bias_add(tf.matmul(tm, kernel_ref), bias_ref)
+
+        # Split result into the four regions mentioned above.
         new_ig, new_gg, new_fg, new_og = tf.split(before_activation_functions, 4, axis=1)
 
+        # Apply appropriate activation functions
         new_ig = tf.sigmoid(new_ig)
         new_fg = tf.sigmoid(new_fg)  # + forget_bias  TODO: what to do with forget bias
         new_gg = tf.tanh(new_gg)
         new_og = tf.sigmoid(new_og)
 
+        # Calculate new state cell
         new_state_cell = tf.add(tf.multiply(new_gg, new_ig),
                                 tf.multiply(new_fg, s))
 
+        # Calculate new hidden state from new cell state and output gate.
         new_hidden_state = tf.multiply(tf.tanh(new_state_cell), new_og)
+
+        # Store all the information that we need.
         hs = hs.write(t, new_hidden_state)
         sc = sc.write(t, new_state_cell)
         ig = ig.write(t, new_ig)
         gg = gg.write(t, new_gg)
         fg = fg.write(t, new_fg)
 
+        # Carry on.
         return tf.add(t, 1), hs, sc, ig, gg, fg
 
+    # Do the while loop.
     i = tf.constant(1)
     time, hs, sc, ig, gg, fg = tf.while_loop(
         cond=lambda i, *_: tf.less_equal(i, sequence_length),
         body=_body,
         loop_vars=[i, hidden_states, state_cells, input_gate, gate_gate, forget_gate])
 
-    # Call lstm lrp with the found information
-    R_new = _calculate_relevance_form_lstm(R[0], weights_gate_gate, bias_gate_gate, input_a, hs, sc, ig, gg, fg, sequence_length)
+    # Call lstm lrp with the recorded information
+    R_new = _calculate_relevance_form_lstm(R[0], weights_gate_gate, bias_gate_gate, input_a, hs, sc, ig, gg, fg,
+                                           sequence_length)
     return tf.expand_dims(R_new, 0)
 
 
-def lstm(ts, R):
-    while_context = []
+def lstm(path, R):
+    """
+    Finds the while context and forwards it along with the
+    relevance and the input tensor for the while context.
+    :param path: the par from current operation to the input
+    :param R: The upper layer relevance
+    :return: The lower layer relevance and the path from just after the while context
+    """
 
-    next_idx = 1
-    next = ts[next_idx]
-    while next.type != 'TensorArrayScatterV3':
-        next = ts.pop(0)
-        while_context.append(next)
+    idx = 0
+    while path[idx].type != 'TensorArrayScatterV3':
+        idx += 1
+    while_context = path[1:idx]
 
-    # Find the transpose operation
-    find_id = next.inputs[2].op._id
-    while next._id != find_id:
-        next = ts.pop(0)
+    # Find the transpose operation in the very beginning
+    find_id = path[idx].inputs[2].op._id
+    while path[idx]._id != find_id:
+        idx += 1
 
+    # Find input to transpose from path for further handeling
+    # by the router
+    find_id = path[idx].inputs[0].op._id
+    while path[idx]._id != find_id:
+        idx += 1
 
-    # Find input to transpose from path but keep operation in pather
-    # for further handeling.
-    find_id = next.inputs[0].op._id
-    while ts[0]._id != find_id:
-        ts.pop(0)
+    # Extract input tensor for while context
+    while_context_input = path[idx].outputs[0]
 
-    while_context_input = ts[0].outputs[0]
+    # Transfar sub graph to the handler of the while context
     R = _handle_while_context(while_context, R, while_context_input)
 
-    return ts, R
+    return path[idx:], R
 
 
-def _calculate_relevance_form_lstm(R, W_g, b_g, X, H, cell_states, input_gate_outputs, gate_gate_outputs, forget_gate_outputs, max_timestep):
+def _linear_lrp_helper(relevance_to_distribute, input, weights, output):
+    activations = tf.multiply(tf.transpose(input), weights)
+    fractions = tf.divide(activations, output)
+    relevances = tf.matmul(relevance_to_distribute, tf.transpose(fractions))
+    return relevances
+
+
+def _calculate_relevance_form_lstm(R, W_g, b_g, X, H, cell_states, input_gate_outputs, gate_gate_outputs,
+                                   forget_gate_outputs, max_timestep):
     """
-    Convolutional lstm
+    Calculated the relevance for an LSTM
     :param tensor: the tensor of the upper activation of the LSTM
     :param R: The upper layer relevance
     :return: lower layer relevance
@@ -190,7 +256,7 @@ def _calculate_relevance_form_lstm(R, W_g, b_g, X, H, cell_states, input_gate_ou
         # linear layers with relevance to distribute = relevance for gate gate in time t,
         # input = (x[t] h[t-1]), weights = W, output = (x[t] h[t-1]) * W.
         h_t_minus_one = H.read(t - 1)
-        x_t = X.read(t - 1)             # Note that X is indexed from 0 where H is from 1
+        x_t = X.read(t - 1)  # Note that X is indexed from 0 where H is from 1
         x_h_concat = tf.concat([x_t, h_t_minus_one], axis=1)
         gate_gate_before_tanh = tf.matmul(x_h_concat, W_g)
 
@@ -198,7 +264,7 @@ def _calculate_relevance_form_lstm(R, W_g, b_g, X, H, cell_states, input_gate_ou
         (rel_xs_t, rel_hs_t_minus_one) = tf.split(rel_x_t_and_h_t_minus_one, [x_depth, units], 1)
 
         rel_xs = rel_xs.write(t, rel_xs_t)
-        rel_hs = rel_hs.write(t-1, rel_hs_t_minus_one)
+        rel_hs = rel_hs.write(t - 1, rel_hs_t_minus_one)
 
         t = tf.add(t, -1)
 

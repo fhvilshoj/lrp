f9a1c1fef965e0a0c7e60579892afa1945b5ae06
Frederik Hvilshøj <fhvilshoj@gmail.com>
2017-10-19 17:04:18 +0200

    Refactored nonlinearities

--- lrp/nonlinearities_lrp.py
+++ lrp/nonlinearities_lrp.py
@@ -1,14 +1,20 @@
-def nonlinearities(path, R):
+import lrp_util
+
+def nonlinearities(router, R):
     """
     Handeling all nonlinearities (Relu, Sigmoid, Tanh) by passing the relevance along
-    :param path: the path of operations towards the input
-    :param R: The upper layer relevance
-    :return: lower layer relevance
+    :param router: the router object to report changes to
+    :param R: the list of tensors containing the relevances from the upper layers
     """
 
-    assert path[0].outputs[0].shape == R.shape, "Tensor and R should have same shape"
+    # Sum the potentially multiple relevances from the upper layers
+    R = lrp_util.sum_relevances(R)
 
-    # Skip this operation and pass R along with the next operation in the path
-    return path[1:], R
+    current_operation = router.get_current_operation()
+    assert current_operation.outputs[0].shape == R.shape, "Tensor and R should have same shape"
 
+    # Report handled operations
+    router.mark_operation_handled(current_operation)
 
+    # Forward the calculated relevance to the input of the convolution
+    router.forward_relevance_to_operation(R, current_operation.inputs[0].op)

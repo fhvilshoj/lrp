57e92399a7b903020e8962cfcfb88cfa961438ff
Benjamin <benjamin-jakobsen@hotmail.com>
2017-10-20 10:46:21 +0200

    Added a few comments to concatenate_lrp

--- lrp/concatenate_lrp.py
+++ lrp/concatenate_lrp.py
@@ -10,24 +10,28 @@ def concatenate(router, R):
     # Get the current concatenate operation
     current_operation = router.get_current_operation()
 
-    # Find axis and order of concatenate
-    print(current_operation)
-
-    # Split relevances in same order
-
+    # Find axis that the concatenation was over
     axis = current_operation.inputs[-1]
+
+    # Split relevances in same order. Start by initializing empty arrays to hold respectively the sizes that the
+    # relevances shall be split in and the receivers of the relevances
     split_sizes = []
     input_operations = []
+    # Run through the inputs to the current operation except the last (the last input is the "axis" input)
     for inp in current_operation.inputs[:-1]:
+        # Add the operation to the array
         input_operations.append(inp.op)
+        # Find the shape of the operation
         shape = tf.shape(inp)
+        # Find and add the size of the input in the "axis" dimension
         split_sizes.append(shape[axis])
 
+    # Split the relevances over the "axis" dimension according to the found split sizes
     R_splitted = tf.split(R, split_sizes, axis)
 
-    # Tell the router that we handled this operation
+    # Tell the router that we handled the concatenate operation
     router.mark_operation_handled(current_operation)
 
-    # Forward relevance to the operation of the input to the current operation
+    # Forward relevance to the operation of the input to the concatenate operation
     for input_index, relevance in enumerate(R_splitted):
         router.forward_relevance_to_operation(relevance, current_operation, input_operations[input_index])

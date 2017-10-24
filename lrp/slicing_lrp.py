f8fdaa66e0b38f407c751022d027931f002855e0
Benjamin <benjamin-jakobsen@hotmail.com>
2017-10-24 18:30:22 +0200

    Fixed lrp for slicing.

--- lrp/slicing_lrp.py
+++ lrp/slicing_lrp.py
@@ -1,41 +1,46 @@
 import lrp_util
 import tensorflow as tf
 
+# TODO: Big fat todo. We don't handle slicing with sizes of -1 at the moment
 def slicing(router, R):
     # Sum the relevances
     R = lrp_util.sum_relevances(R)
 
+    R = tf.Print(R, [R], message="R: ", summarize=1000)
     # Get the current operation
     current_operation = router.get_current_operation()
 
     # Get the input to the slicing operation
     input_to_slicing_operation = current_operation.inputs[0]
 
+    # Cut off the batch size and maybe predictions pr. sample if exists from the tensor shapes
+    # since we never want to pad the batch dimension
+    free_dimensions = 2 - (tf.rank(R) - tf.rank(input_to_slicing_operation))
+    def _relevant_dims(tensor):
+        return tensor[free_dimensions:]
+
     # Get the shape of the input to the slicing operation
-    input_shape = tf.shape(input_to_slicing_operation)
-    print(input_shape)
+    input_shape = _relevant_dims(tf.shape(input_to_slicing_operation))
 
     # Find starting point of the slice operation
-    starting_point = current_operation.inputs[1]
-    print(starting_point)
+    starting_point = _relevant_dims(current_operation.inputs[1])
 
     # Find size of the slice operation
-    size_of_slice = current_operation.inputs[2]
-    print(size_of_slice)
+    size_of_slice = _relevant_dims(current_operation.inputs[2])
 
     # Find the number of zeros to insert after the relevances
     end_zeros = input_shape - (starting_point + size_of_slice)
 
-    print(end_zeros)
-
     # For each axis, insert 'starting_point' zeros before the relevances and
     # 'size_of_split'-('starting_point' + 'size_of_split') zeros after the relevances
     padding = tf.transpose(tf.stack([starting_point, end_zeros]))
-    R = tf.pad(R, padding)
+    batch_and_sample_padding = tf.zeros((free_dimensions, 2), dtype=tf.int32)
+    padding = tf.concat([batch_and_sample_padding, padding], axis=0)
+
+    R_new = tf.pad(R, padding)
 
     # Mark operations as handled
     router.mark_operation_handled(current_operation)
 
     # Forward the calculated relevances
-    router.forward_relevance_to_operation(R, current_operation, input_to_slicing_operation.op)
-
+    router.forward_relevance_to_operation(R_new, current_operation, input_to_slicing_operation.op)
--- test/test_slicing.py
+++ test/test_slicing.py
@@ -11,15 +11,13 @@ class TestSlicing(unittest.TestCase):
                                [[11, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]],
                                [[1, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]]], dtype=tf.float32)
 
-            out = tf.slice(inp, [2, 0, 0], [1, 3, 4])
+            out = tf.slice(inp, [0, 0, 2], [-1, -1, 2])
 
             R = lrp.lrp(inp, out)
-
             with tf.Session() as s:
-                expected_explanation = np.array([[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
-                                                 [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
-                                                 [[0, 2, 0, 0], [3, 0, 0, 0], [4, 0, 0, 0]]])
+                expected_explanation = np.array([[[0, 0, 0, 4], [0, 0, 0, 8], [0, 0, 0, 3]],
+                                                 [[0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0]],
+                                                 [[0, 0, 2, 0], [0, 0, 3, 0], [0, 0, 4, 0]]])
                 explanation = s.run(R)
-
                 self.assertTrue(np.allclose(expected_explanation, explanation, atol=1e-03, rtol=1e-03),
                                 "The explanation does not match the expected explanation")

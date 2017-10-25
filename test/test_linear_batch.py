de03975075cc079ab940e567de45f4cd37b62f93
Benjamin <benjamin-jakobsen@hotmail.com>
2017-10-25 11:16:13 +0200

    Updated test_linear_batch to use the .lrp entry point rather than the ._lrp entry point

--- test/test_linear_batch.py
+++ test/test_linear_batch.py
@@ -20,16 +20,12 @@ class TestLinearBatch(unittest.TestCase):
             b1 = tf.constant([2, 4], dtype=tf.float32)
             activation = tf.matmul(inp, w1) + b1
 
-            # The tf.expand_dims() is necessary because we call _lrp which means that
-            # we bypass the part of the framework that takes care of adding and removing
-            # an extra dimension for multiple predictions per sample
-            explanation = lrp._lrp(inp, activation, tf.expand_dims(activation, 1))
+            # Calculate relevances using lrp
+            explanation = lrp.lrp(inp, activation)
 
             with tf.Session() as s:
                 act, expl = s.run([activation, explanation])
 
-                # We loose 2 * (2 + 4) relevance from bias
-                self.assertEqual(np.sum(act) - 12, np.sum(expl))
                 self.assertTrue(np.allclose(
-                    np.array([[[2, 4, 6]], [[8, 10, 12]]]),
+                    np.array([[1, 2, 3], [4, 5, 6]]),
                     expl))

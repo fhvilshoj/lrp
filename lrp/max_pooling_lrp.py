06af2d58eab177002b46e43ca6dbb5fff58d9b34
Benjamin <benjamin-jakobsen@hotmail.com>
2017-10-25 17:27:38 +0200

    Updated max_pooling_lrp to work with the new structure, where relevances that flow through the framework always have batch_size, predictions_per_sample as the two first dimensions

--- lrp/max_pooling_lrp.py
+++ lrp/max_pooling_lrp.py
@@ -10,15 +10,18 @@ def max_pooling(router, R):
     :param R: the list of tensors containing the relevances from the upper layers
     """
     # Sum the potentially multiple relevances from the upper layers
+    # Shape of R: (batch_size, predictions_per_sample, out_height, out_width, out_depth)
     R = lrp_util.sum_relevances(R)
 
     # Get current operation from the router
     current_operation = router.get_current_operation()
 
-    # max pool output as current tensor
+    # Get the output from the max pool operation
+    # Shape of current_tensor: (batch_size, out_height, out_width, out_depth)
     current_tensor = current_operation.outputs[0]
 
     # Find the input to the max pooling
+    # Shape of current_tensor: (batch_size, in_height, in_width, in_depth)
     max_pool_input = current_operation.inputs[0]
 
     # Find the padding and strides that were used in the max pooling
@@ -29,52 +32,72 @@ def max_pooling(router, R):
     # Get shape of the input
     (batch_size, input_height, input_width, input_channels) = max_pool_input.get_shape().as_list()
 
-    # Get the shape of the output of the output of the max pool
+    # Get the shape of the output of the max pool operation
     (_, output_height, output_width, output_channels) = current_tensor.get_shape().as_list()
 
-    # Reshape the relevances to shape (batch_size * predictions_per_sample, output_height, output_width, output_channels)
-    # instead of (batch_size, predictions_per_sample, output_height, output_width, output_channels) ease the following
-    # lrp calculations
-    relevances_shape = R.get_shape().as_list()
-    predictions_per_sample = relevances_shape[1]
-    batch_size_times_predictions_per_sample = batch_size * predictions_per_sample
-    R = tf.reshape(R, (batch_size_times_predictions_per_sample, output_height, output_width, output_channels))
-
     # Replace the negative elements with zeroes to only have the positive entries left
+    # Shape of max_pool_input_positive: (batch_size, in_height, in_width, in_depth)
     max_pool_input_positive = lrp_util.replace_negatives_with_zeros(max_pool_input)
 
-    # Extract every patch of the input (i.e. portion of the input that the kernel looks at a
-    # time), to get a tensor of shape
-    # (batch, out_height, out_width, kernel_height*kernel_width*input_channels)
+    # Extract every patch of the input (i.e. portion of the input that the kernel looks at a time)
+    # Shape of image_patches: (batch, out_height, out_width, kernel_height*kernel_width*input_channels)
     image_patches = tf.extract_image_patches(max_pool_input_positive, kernel_size,
                                              strides, [1, 1, 1, 1], padding)
+
+    # Reshape image patches to "small images" instead of lists
+    # Shape of image_patches after reshape:
+    # (batch_size, out_height, out_width, kernel_height, kernel_width, input_channels)
     image_patches = tf.reshape(image_patches, [batch_size, output_height, output_width, kernel_size[1], kernel_size[2], input_channels])
 
     # Find the largest elements in each patch and set all other entries to zero (to find z_ijk+'s)
+    # Shape of max_elems: (batch_size, out_height, out_width, 1, 1, input_channels)
     max_elems = tf.reduce_max(image_patches, axis=[3, 4], keep_dims=True)
+
+    # Select maximum in each patch and set all others to zero
+    # Shape of zs: (batch_size, out_height, out_width, kernel_height, kernel_width, input_channels)
     zs = tf.where(tf.equal(image_patches, max_elems), image_patches, tf.zeros_like(image_patches))
 
     # Find the contribution of each feature in the input to the activations,
     # i.e. the ratio between the z_ijk's and the z_jk's (plus a small stabilizer to avoid division by zero)
     # TODO We do not take care of two equaly big entries (currently doubles given relevance)
-    fraction = zs / (max_elems + EPSILON)
+    # Shape of fractions: (batch_size, out_height, out_width, kernel_height, kernel_width, input_channels)
+    fractions = zs / (max_elems + EPSILON)
 
-    # Find the relevance of each feature
-    R = tf.reshape(R, [batch_size_times_predictions_per_sample, output_height, output_width, 1, 1, output_channels])
+    # Add the predictions_per_sample dimension to be able to broadcast fractions over the different
+    # predictions for the same sample
+    # Shape after expand_dims:
+    # (batch_size, predictions_per_sample=1, out_height, out_width, kernel_height, kernel_width, input_channels)
+    fractions = tf.expand_dims(fractions, 1)
 
-    relevances = fraction * R
+    # Extract information about R for later reshapes
+    relevances_shape = R.get_shape().as_list()
+    predictions_per_sample = relevances_shape[1]
+    batch_size_times_predictions_per_sample = batch_size * predictions_per_sample
+
+    # Put the relevance for each patch in the dimension that corresponds to the "input_channel" dimension
+    # of the fractions
+    # Shape of R after reshape: (batch_size, predictions_per_sample, out_height, out_width, 1, 1, out_channels)
+    R = tf.reshape(R, [batch_size, predictions_per_sample, output_height, output_width, 1, 1, output_channels])
+
+    # Distribute the relevance onto athe fractions
+    # Shape of new relevances: (batch_size, out_height, out_width, kernel_height, kernel_width, input_channels)
+    relevances = fractions * R
+
+    # Put the batch size and predictions_per_sample on the same dimension to be able to use the patches_to_images tool.
+    # Also rearrange patches back to lists from the "small images".
+    # Shape of relevances after reshape:
+    # (batch_size * predictions_per_sample, out_height, out_width, kernel_height * kernel_width * input_channels)
     relevances = tf.reshape(relevances, (batch_size_times_predictions_per_sample, output_height, output_width,
                                          kernel_size[1]*kernel_size[2] * input_channels))
 
-    # Reconstruct the shape of the input, thereby summing the relevances for each individual pixel
-    R_new = lrp_util.patches_to_images(relevances, batch_size, input_height, input_width, input_channels, output_height,
+    # Reconstruct the shape of the input, thereby summing the relevances for each individual feature
+    R_new = lrp_util.patches_to_images(relevances, batch_size_times_predictions_per_sample, input_height, input_width, input_channels, output_height,
                                        output_width, kernel_size[1], kernel_size[2], strides[1], strides[2], padding)
 
-    # Reshape the calculated relevances to shape
+    # Reshape the relevances back to having batch_size and predictions_per_sample as the first two dimensions
     # (batch_size, predictions_per_sample, input_height, input_width, input_channels) rather than
     # (batch_size * predictions_per_sample, input_height, input_width, input_channels)
     R_new = tf.reshape(R_new, (batch_size, predictions_per_sample, input_height, input_width, input_channels))
-
 
     # Report handled operations
     router.mark_operation_handled(current_operation)

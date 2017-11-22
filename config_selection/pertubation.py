9dfea55049da7f6aae4f01652f27eb62c60a1ac8
Frederik Hvilshøj <fhvilshoj@gmail.com>
2017-11-22 16:03:23 +0100

    Updated input perturbation to take batches of samples

--- config_selection/pertubation.py
+++ config_selection/pertubation.py
@@ -2,8 +2,10 @@ import tensorflow as tf
 
 from config_selection import logger
 
+
 class Pertuber(object):
     def __init__(self, X, R, **kwargs):
+        self.args = kwargs
 
         self.X = X
         self.R = R
@@ -18,76 +20,166 @@ class Pertuber(object):
     def _prepare_priority_array(self):
         logger.debug("Preparing priority array")
 
-        # Find number of pertubations to do
-        active_indices = tf.size(self.R.values)
-        self.pertubations = tf.minimum(active_indices, self.num_iterations)
+        self.batch_size = self.args['batch_size']
 
-        # Prepare a tensorarray with elements condaining the ordered indices
-        # of the most relevant features
-        ta = tf.TensorArray(tf.int64, self.pertubations, clear_after_read=True, infer_shape=False, element_shape=(3,))
+        # Find the active count for each sample by first making an indicator tensor
+        indicatores = tf.SparseTensor(self.R.indices, tf.ones_like(self.R.values), self.R.dense_shape)
 
-        # Find the ordered top relevances
-        _, top_indices = tf.nn.top_k(self.R.values, k=self.pertubations)
+        # Then summing over everything but the batch dimension
+        self.counts = tf.cast(tf.sparse_reduce_sum(indicatores, axis=[1, 2]), dtype=tf.int32)
 
-        # Store the associated indices to use later
-        indices = tf.gather(self.R.indices, top_indices)
-        self.priority_indices = ta.unstack(indices)
+        # Split indices of R into a tensor array with respect to the sizes of the samples
+        indices_ta = tf.TensorArray(tf.int64, self.batch_size, dynamic_size=False, clear_after_read=True,
+                                    infer_shape=False, element_shape=(None, 3))
+        indices_ta = indices_ta.split(self.R.indices, self.counts)
+
+        # Split values of R into a tensor array with respect to the sizes of the samples
+        values_ta = tf.TensorArray(tf.float32, self.batch_size, dynamic_size=False, clear_after_read=True,
+                                   infer_shape=False, element_shape=(None,))
+        values_ta = values_ta.split(self.R.values, self.counts)
+
+        # Prepare tensorarray to hold ordered indices for each sample
+        ordered_indices_ta = tf.TensorArray(tf.int64, self.batch_size, dynamic_size=False, clear_after_read=True,
+                                            infer_shape=True)
+
+        def _sort_body(t, ordered_ta):
+
+            # Read indices and values from tensor arrays
+            sample_indices = indices_ta.read(t)
+            sample_values = values_ta.read(t)
+
+            # Find size of values
+            sample_size = tf.size(sample_values)
+
+            # If more values than num_iterations stick with num_iterations
+            sample_size = tf.minimum(sample_size, self.num_iterations)
+
+            # Find the ordered top relevances
+            _, top_indices = tf.nn.top_k(sample_values, k=sample_size)
+
+            # Store the associated indices to use later
+            indices = tf.gather(sample_indices, top_indices)
+
+            def _do_padding():
+                to_pad = self.num_iterations - sample_size
+                padding = tf.tile(tf.constant([[-1, -1, -1]], dtype=tf.int64), [to_pad, 1])
+                return tf.concat([indices, padding], axis=0)
+
+            def _skip_padding():
+                return indices
+
+            # Indices shape: (num_iterations, 3)
+            indices = tf.cond(tf.less(sample_size, self.num_iterations),
+                              true_fn=_do_padding,
+                              false_fn=_skip_padding)
+
+            ordered_ta = ordered_ta.write(t, indices)
+
+            return t + 1, ordered_ta
+
+        _, ordered_indices_ta = tf.while_loop(
+            cond=lambda t, _: tf.less(t, self.batch_size),
+            body=_sort_body,
+            loop_vars=[0, ordered_indices_ta]
+        )
+
+        # Ordered_indices_stackes shape: (batch_size, num_iterations, 3)
+        ordered_indices_stacked = ordered_indices_ta.stack()
+
+        # Transpose to group respectively the most important, next most important, etc. together
+        ordered_indices_transposed = tf.transpose(ordered_indices_stacked, [1, 0, 2])
+
+        # Reshape to have shape (batch_size * num_iterations, 3)
+        self.priority_indices = tf.reshape(ordered_indices_transposed, (self.batch_size * self.num_iterations, 3))
 
     def build_pertubation_graph(self, template):
         logger.debug("Building pertubation_graph")
         results_ta = tf.TensorArray(tf.float32, self.num_iterations + 1)
+        priority_indices_ta = tf.TensorArray(tf.int64, self.batch_size * self.num_iterations,
+                                          dynamic_size=False,
+                                          clear_after_read=True).unstack(self.priority_indices)
 
-        def _pertubation_loop_body(t, X_values, res_ta):
+        def _pertubation_loop_body(i, X_values, res_ta):
             #### Do and record forward pass
-            # res shape: (2,)
             X = tf.SparseTensor(self.X.indices, X_values, self.X.dense_shape)
             model = template(X)
-            res_ta = res_ta.write(t, model['y_hat'])
+            res_ta = res_ta.write(i, model['y_hat'])
 
-            def _prepare_next_X():
-                ####  Do pertubation
-                # Find index to set to 0
-                index = self.priority_indices.read(t)
+            def _prepare_next_iteration():
+                # Loop for pertubing batch_size times
+                def _pertubation_body(j, current_selection):
+                    ####  Do pertubation
+                    # Find index to set to 0
 
-                # Make selection from index and indices in X by first finding all partial matches with the index:
-                # Selection shape: (sequence_length, 3)
-                selection = tf.where(tf.equal(X.indices, index), tf.ones_like(X.indices), tf.zeros_like(X.indices))
+                    def _select_index():
+                        idx = i * self.batch_size + j
+                        index = priority_indices_ta.read(idx)
 
-                # Next sum matches to se if we can get a sum of 3 indicating that all three dimensions, i.e.
-                # (sample, time_step, feature) are matching.
-                # selection shape: (sequence_length,)
-                selection = tf.reduce_sum(selection, axis=1)
+                        # Make selection from index and indices in X by first finding all partial matches with the index:
+                        # Selection shape: (sequence_length, 3)
+                        selection = tf.where(tf.equal(X.indices, index), tf.ones_like(X.indices), tf.zeros_like(X.indices))
 
-                # Filter out that one index to remove by setting that index to 0
-                selection = tf.where(tf.equal(selection, 3),
-                                     tf.zeros_like(selection, dtype=tf.float32),
-                                     tf.ones_like(selection, dtype=tf.float32))
+                        # Next sum matches to se if we can get a sum of 3 indicating that all three dimensions, i.e.
+                        # (sample, time_step, feature) are matching.
+                        # selection shape: (sequence_length,)
+                        selection = tf.reduce_sum(selection, axis=1)
 
-                # Used the filter to multiply elementwise between old values and the filter
-                return X.values * selection
+                        # Filter out that one index to remove by setting that index to 0
+                        selection = tf.where(tf.equal(selection, 3),
+                                             tf.ones_like(selection, dtype=tf.float32),
+                                             tf.zeros_like(selection, dtype=tf.float32))
+                        return selection
+
+                    def _skip_index():
+                        skipping = tf.zeros_like(current_selection)
+                        return skipping
+
+                    # Only select index if there are more values in the given sample
+                    selection = tf.cond(
+                        tf.less(i, self.counts[j]),
+                        _select_index,
+                        _skip_index
+                    )
+
+                    # Used the filter to multiply elementwise between old values and the filter
+                    return j + 1, current_selection + selection
+
+                _, selection = tf.while_loop(
+                    cond=lambda j, _: tf.less(j, self.batch_size),
+                    body=_pertubation_body,
+                    loop_vars=[0, tf.zeros_like(X_values)]
+                )
+
+                # Invert selection to be able to multiply it with the X_values
+                selection = tf.where(tf.equal(selection, 0),
+                                     tf.ones_like(selection),
+                                     tf.zeros_like(selection))
+
+
+                # Remove selected values
+                return X_values * selection
 
             def _last_iteration():
-                return X.values
+                # The return of this is never used so it could be whatever
+                return X_values
 
-            X_values_new = tf.cond(tf.less(t, self.pertubations),
-                            true_fn=_prepare_next_X,
-                            false_fn=_last_iteration)
+            X_values_new = tf.cond(tf.equal(i, self.num_iterations),
+                                   true_fn=_last_iteration,
+                                   false_fn=_prepare_next_iteration)
 
-            return t + 1, X_values_new, res_ta
+            return i + 1, X_values_new, res_ta
 
         *_, results_ta = tf.while_loop(
-            cond=lambda t, *_: tf.less_equal(t, self.pertubations),
+            cond=lambda t, *_: tf.less_equal(t, self.num_iterations),
             body=_pertubation_loop_body,
             loop_vars=[0, self.X.values, results_ta]
         )
 
         # Stack results to a tensor
-        # shape: (pertubations, 2)
-        results = results_ta.gather(tf.range(0, self.pertubations + 1))
-        result = tf.squeeze(results, 1)
-        result = tf.transpose(result)
+        # shape: (num_iterations, batch_size, 2)
+        range = tf.range(0, self.num_iterations + 1)
+        results = results_ta.gather(range)
+
+        # Transpose result to shape (batch_size, num_iterations, num_classes)
+        result = tf.transpose(results, [1, 0, 2])
         return result
-
-
-
-
--- config_selection/test_pertubation.py
+++ config_selection/test_pertubation.py
@@ -12,20 +12,27 @@ class PertubationTest(unittest.TestCase):
                 [0, 1, 1],
                 [0, 1, 3],
                 [0, 1, 4],
-                [0, 2, 2]
+                [0, 2, 2],
+
+                [1, 1, 1],
+                [1, 1, 3],
+                [1, 1, 4],
+                [1, 2, 2]
             ], dtype=tf.int64)
-            X_values = [1, 1, 0, 2.]
-            X = tf.SparseTensor(X_indices, X_values, (1, 4, 5))
+            X_values = [1, 1, 0, 2., 1, 1, 0, 2.]
+            X = tf.SparseTensor(X_indices, X_values, (2, 4, 5))
 
             R_indices = tf.constant([
                 [0, 1, 1],
-                [0, 1, 3]
+                [0, 1, 3],
+                [1, 1, 1],
+                [1, 1, 3]
             ], dtype=tf.int64)
-            R_values = [0.2, 0.3]
-            R = tf.SparseTensor(R_indices, R_values, (1, 4, 5))
+            R_values = [0.2, 0.3, 0.2, 0.3]
+            R = tf.SparseTensor(R_indices, R_values, (2, 4, 5))
 
             def _scale_sum(x):
-                x = tf.sparse_reshape(x, (4, 5))
+                x = tf.sparse_reshape(x, (8, 5))
                 scalar = tf.get_variable('scalar',
                                          dtype=tf.float32,
                                          shape=(5, 2),
@@ -34,9 +41,13 @@ class PertubationTest(unittest.TestCase):
                                              dtype=tf.float32))
                 # shape = (5, 2)
                 mult = tf.sparse_tensor_dense_matmul(x, scalar)
+                mult = tf.reshape(mult, (2, 4, 2))
 
+                # mult = tf.Print(mult, [mult], summarize=100)
                 # reduce sum
-                res = tf.squeeze(tf.reduce_sum(mult, 0))
+                # shape = (1, 2)
+                res = tf.reduce_sum(mult, 1)
+                # res = tf.Print(res, [res], "RES: ", summarize=10)
                 return {'y_hat': res}
 
             scale_sum = tf.make_template('scale_sum', _scale_sum)
@@ -45,14 +56,16 @@ class PertubationTest(unittest.TestCase):
                 # Test to see that we can do decorators around templates
                 return scale_sum(x)
 
-            pertuber = Pertuber(X, R, 3)
+            pertuber = Pertuber(X, R, **{'batch_size': 2, 'pertubations': 2})
 
             res = pertuber.build_pertubation_graph(_some_other)
 
             with tf.Session() as s:
                 s.run(tf.global_variables_initializer())
                 results = s.run(res)
-                expected = np.array([[2, 2], [2, 1], [2, 0]])
+                expected = np.array([[[2, 2], [2, 1], [2, 0]], [[2, 2], [2, 1], [2, 0]]])
+
+                print(results)
 
                 # Check for shape and actual result
                 self.assertEqual(expected.shape, results.shape,

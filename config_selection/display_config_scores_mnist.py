aca55b7f6f66cd37f9d7976c1f377c2bf1ee7bcc
Frederik Hvilshøj <fhvilshoj@gmail.com>
2017-12-02 15:51:44 +0100

    Added mnist result parsers

---
+++ config_selection/display_config_scores_mnist.py
@@ -0,0 +1,82 @@
+import argparse
+import numpy as np
+from matplotlib import pyplot as plt
+
+from os import listdir
+from os.path import isfile, join
+
+from config_selection import logger
+from config_selection.score_parser_mnist import ScoreParser
+
+
+def _write_scores_to_plot(scores, destination, **kwargs):
+    def _add_plot(score, title):
+        to_plot = np.concatenate([[0], np.mean(score.pertubation_scores, axis=0)], axis=0)
+        plt.plot(np.arange(score.pertubations + 1), to_plot, label=title, marker=kwargs['marker'])
+
+    if kwargs['line_titles']:
+        for (score, title) in zip(scores, kwargs['line_titles']):
+            _add_plot(score, title)
+    else:
+        for score in scores:
+            _add_plot(score, score.short_description())
+
+    plt.ylabel('Score differences')
+    plt.xlabel('Pertubations')
+    if len(kwargs['plot_title']) > 0:
+        plt.title(kwargs['plot_title'])
+
+    plt.legend(loc=2)
+
+    plt.savefig(destination, bbox_inches='tight')
+    logger.debug("Graph generated at {}".format(destination))
+
+
+def _show_results(**kwargs):
+    if kwargs['file']:
+        scores = [ScoreParser(f) for f in kwargs['file']]
+    else:
+        scores = [ScoreParser(join(kwargs['benchmark_dir'], f)) for f in listdir(kwargs['benchmark_dir']) if
+                  isfile(join(kwargs['benchmark_dir'], f)) and f.endswith(".res")]
+
+    scores.sort(key=lambda x: x.AOPC, reverse=True)
+    to_print = [sc for sc in scores if sc.title in ['Sensitivity Analysis', 'Random']]
+    rest = [sc for sc in scores if sc.title not in ['Sensitivity Analysis', 'Random']]
+
+    to_print.extend(rest[:max(10, len(rest))])
+
+    for score in to_print:
+        logger.info("--------------------------------------------------")
+        logger.info("\n" + str(score))
+
+    if kwargs['plot']:
+        _write_scores_to_plot(to_print, **kwargs)
+
+
+def _main():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='Get model scores')
+    parser.add_argument('-b', '--benchmark_dir', type=str,
+                        default='E:/frederikogbenjamin/benchmarks/lrp_vs_sa',
+                        help='The location of the benchmark results')
+    parser.add_argument('-f', '--file', nargs="*", default=[],
+                        help='Files to display scores for')
+    parser.add_argument('-d', '--destination')
+
+    # Extra options
+    parser.add_argument('--plot', action='store_true',
+                        help='Boolean indicator. If true plots are generated.')
+    parser.add_argument('-l', '--line-titles', type=str, nargs="*", default=[],
+                        help='Titles to be used in plot')
+    parser.add_argument('-m', '--marker', type=str, default='.')
+    parser.add_argument('-t', '--plot-title', type=str, default='',
+                        help='Title to be displayed in top of plot')
+
+    args = parser.parse_args()
+
+    # Call config selection with gathered arguments
+    _show_results(**vars(args))
+
+
+if __name__ == '__main__':
+    _main()
---
+++ config_selection/score_parser_mnist.py
@@ -0,0 +1,156 @@
+import re
+import numpy as np
+from lrp.configuration import *
+
+# Lookup at linear, batch_norm, sparse, convolution, max_pool, lstm
+type_config_re = r'.*/LIN_(?P<linear>.*)_ELE_(?P<batch_norm>.*)_SPA_(?P<sparse>.*)_CONV_(?P<convolution>.*)_MAX_(?P<max_pool>.*)_LSTM_(?P<lstm>.*)'
+type_config_sm_re = r'.*/LIN_(?P<linear>.*)_ELE_(?P<batch_norm>.*)_SPA_(?P<sparse>.*)_CONV_(?P<convolution>.*)_MAX_(?P<max_pool>.*)_LSTM_(?P<lstm>.*)_SM_(?P<softmax>.*)\.res'
+
+# Lookup at alpha, epsilon, bias, winners_take_all, flat, ww
+rules_re = r'(((a(?P<alpha>\-{0,1}[0-9]+\.*[0-9]*)b(?P<beta>\-{0,1}[0-9]+\.*[0-9]*))|(e(?P<epsilon>[0-9]+\.*[0-9]*)))_(?P<bias>\w\w))|(?P<winners_take_all>win)|(?P<flat>flat)|(?P<ww>ww)'
+
+# File name
+file_name_re = r'.*/(LIN.*\.res)'
+
+bias_strategies = {
+    'no': 'Absorb',
+    'ig': 'Ignore',
+    'ac': 'Active',
+    'al': 'All'
+}
+
+class Config(object):
+
+    def __init__(self, layer, conf_string):
+        rule_dict = re.match(rules_re, conf_string).groupdict()
+
+        self.layer = layer.replace("_", " ").title()
+        self.bias_strategy = None
+
+        if rule_dict['alpha'] is not None:
+            self.rule = 'Alpha {alpha} Beta {beta}'.format(**rule_dict)
+            self.bias_strategy = bias_strategies[rule_dict['bias']]
+        elif rule_dict['epsilon'] is not None:
+            self.rule = 'Epsilon {epsilon}'.format(**rule_dict)
+            self.bias_strategy = bias_strategies[rule_dict['bias']]
+        elif rule_dict['flat'] is not None:
+            self.rule = 'Flat'
+        elif rule_dict['winners_take_all'] is not None:
+            self.rule = 'Winners take all'
+        else:
+            self.rule = 'WW'
+
+    def __str__(self):
+        s = "{:12}: {}".format(self.layer, self.rule)
+        if self.bias_strategy is not None:
+            s += ", bias: {}".format(self.bias_strategy)
+        return s
+
+
+class ScoreParser(object):
+
+    def __init__(self, score_file) -> None:
+        self.layer_configurations = []
+        self.score_file = score_file
+        self.samples = 0
+        self.pertubations = 0
+        self.classes = 0
+        self.AOPC = 0
+        self.title = ""
+
+        self._infer_config_from_file_name(score_file)
+        self._find_shapes(score_file)
+        self._parse_file(score_file)
+
+    def __str__(self) -> str:
+        res = ""
+        if self.layer_configurations:
+            for config in self.layer_configurations:
+                res += "{}\n".format(config)
+        else:
+            res += self.title + "\n"
+        res += "{:12}: {}\n".format('File', re.sub(file_name_re, r'\1', self.score_file))
+        res += "{:12}: {:10f}".format('AOPC', self.AOPC)
+        return res
+
+    def short_description(self) -> str:
+        return re.sub(file_name_re, r'\1', self.score_file)
+
+    def get_AOPC(self) -> float:
+        return self.AOPC
+
+    def _infer_config_from_file_name(self, score_file) -> None:
+        if 'random' in score_file:
+            self.title = 'Random'
+            return
+        elif 'sensitivity' in score_file:
+            self.title = 'Sensitivity Analysis'
+            return
+
+        if "SM" in score_file:
+            layer_dict = re.match(type_config_sm_re, score_file).groupdict()
+        else:
+            layer_dict = re.match(type_config_re, score_file[:-4]).groupdict()
+
+        for (layer, conf_string) in layer_dict.items():
+            if layer == 'sparse':
+                continue
+            self.layer_configurations.append(Config(layer, conf_string))
+        self.layer_configurations.sort(key=lambda x: x.layer)
+
+    def _find_shapes(self, score_file) -> None:
+        with open(score_file, 'r') as sf:
+            headings = sf.readline().split()
+            column_count = len(headings)
+            self.pertubations = column_count - 3
+
+            sample_count = 0
+            classes = 0
+            state = True
+            for line in sf:
+                if len(line) < 10:
+                    if state:
+                        # Blank line
+                        sample_count += 1
+                        self.classes = classes
+                        classes = 0
+                        state = False
+                else:
+                    classes += 1
+                    state = True
+            self.samples = sample_count
+
+    def _parse_file(self, score_file):
+        labels = []
+        predictions = []
+        x0_predictions = []
+        pertubation_scores = []
+
+        with open(score_file, 'r') as sf:
+            # Read headings
+            sf.readline()
+            for i in range(self.samples):
+                predicted_class = 0
+                line = sf.readline()
+                split = line.split()
+                labels.append(int(split[0]))
+                predicted_class = int(split[1])
+                predictions.append(predicted_class)
+                
+                x0_predictions.append(float(split[2]))
+                pertubation_scores.append([float(k) for k in split[3:]])
+
+                # Read blank line
+                sf.readline()
+
+        self.labels = np.array(labels)
+        self.predictions = np.array(predictions)
+        self.x0_predictions = np.array(x0_predictions)
+
+        # Shape (samples, pertubations)
+        self.pertubation_scores = np.array(pertubation_scores)
+        self.pertubation_scores = self.x0_predictions.reshape((self.samples, 1)) - self.pertubation_scores
+
+        self.AOPC = np.mean(np.sum(self.pertubation_scores, 1)) / (1 + self.pertubations)
+
+

1899ac4ac6a26a1a1223459c873e8eb4cbc0ef08
Frederik Hvilshøj <fhvilshoj@gmail.com>
2017-12-05 13:29:12 +0100

    Added file to find known input and one to parse the found input

---
+++ config_selection/find_inputs.py
@@ -0,0 +1,132 @@
+import tensorflow as tf
+import numpy as np
+import argparse
+import re
+from os import listdir, makedirs
+from os.path import isfile, join, exists
+from config_selection.feature_parser import FeatureParser
+
+# Disable tf warnings
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+dir_name_re = r'(.*\/).*'
+single_feature_size = 4512
+context_size = 123
+batch_size = 1
+
+# File name
+def _create_dir_if_not_exists(destination_file):
+    if exists(destination_file):
+        raise ValueError("{} already exists".format(destination_file))
+    else:
+        dir = re.sub(dir_name_re, r'\1', destination_file)
+        if not exists(dir):
+            makedirs(dir)
+        if not destination_file.endswith(".npy"):
+            destination_file = re.sub(r'(.*)\..*', r'\1.npy', destination_file)
+        return destination_file
+
+def _read_sparse_tensor(spt):
+    with open(spt, 'r') as s:
+        shape = [int(i) for i in s.readline().split()]
+        s.readline()
+        indices = []
+        for i in range(len(shape)):
+            dimension = [int(d) for d in s.readline().split()]
+            indices.append(dimension)
+        indices = np.array(indices)
+        indices = indices.transpose()
+        s.readline()
+        values = np.array([float(d) for d in s.readline().split()])
+        return indices, values, shape
+        
+
+def _read_sparse_tensors(spt):
+    sparse_indices = []
+    sparse_values = []
+    sparse_shapes = []
+    
+    for sp in spt:
+        i, v, s = _read_sparse_tensor(sp)
+        first_dim = i.transpose().reshape(-1)[:len(i)]
+        
+        for sample in range(s[0]):
+            selection = np.where(first_dim == sample)
+            selected_indices = i[selection]
+            selected_indices[:,0] = 0
+            sparse_indices.append(selected_indices)
+            sparse_values.append(v[selection])
+            sparse_shapes.append(np.concatenate([[1], s[1:]], axis=0))
+    return sparse_indices, sparse_values, sparse_shapes
+
+
+def _search_for_tensors(indices, values, shapes, feature_file, **kwargs):
+
+    with tf.Graph().as_default():
+        f_parser = FeatureParser(feature_file, single_feature_size, context_size, batch_size)
+        next_batch = f_parser.next_batch()
+        found_tensors = []
+        permutation = []
+        with tf.Session() as s:
+            s.run(tf.local_variables_initializer())
+            coord = tf.train.Coordinator()
+            tf.train.start_queue_runners(sess=s, coord=coord)
+
+            sum = 0
+            i = 0
+            found = 0
+            while f_parser.has_next():
+                r = s.run(next_batch)
+                labels = r['label'].transpose()
+                sum += np.sum(labels)
+
+                read_vals = r['features'].values
+                for j, val in enumerate(values):
+                    if len(val) == len(read_vals) and np.allclose(val, read_vals):
+                        print("Found sample at index {}".format(i))
+                        r['permutation'] = j
+                        found_tensors.append(r)
+                        found += 1
+
+                if found == len(values):
+                    break                
+                i += 1
+                f_parser.did_read_batch()
+
+            coord.request_stop()
+            coord.join()
+            
+            found_tensors = found_tensors[::-1]
+            found_tensors.sort(key=lambda x: x['permutation'])
+           
+            print("Found {} matching".format(found))
+
+            return found_tensors
+
+def _write_found_tensors_to_file(found_tensors, destination):
+    print("Writing tensors into %s \n -- In sorted order" % destination)
+    np.save(destination, found_tensors)
+    
+
+def main():
+    parser = argparse.ArgumentParser(description='Use this tool to read .spt files and search .txt files for equal tensors')
+    parser.add_argument('-f', '--feature-file', type=str, nargs=1,
+                        default='E:/frederikogbenjamin/sepsis/data/TODO')
+    parser.add_argument('-s', '--spt', type=str, nargs='+',
+                        default=['E:/fraderikogbenjamin/benchmarks/TODO'])
+    parser.add_argument('-d', '--destination', type=str,
+                        default='E:/frederikogbenjamin/benchmarks/pickles/TODO')
+
+    args = parser.parse_args()
+
+    dest = _create_dir_if_not_exists(args.destination)
+
+    si, sv, ss = _read_sparse_tensors(args.spt)
+
+    found_tensors = _search_for_tensors(si, sv, ss, **vars(args))
+
+    _write_found_tensors_to_file(found_tensors, dest)
+
+if __name__ == '__main__':
+    main()
---
+++ config_selection/npy_feature_parser.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+import numpy as np
+
+class NpyFeatureParser(object):
+    def __init__(self, feature_file, batch_size, *args, **kwargs):
+        self.tensors = np.load(feature_file[0])
+        self.batch_size = batch_size
+        self.tensors = self._merge_tensors_to_batch_size(self.tensors, self.batch_size)
+        self.record_count = len(self.tensors)
+        self.records_read = 0
+
+    def _merge_models(self, m1, m2):
+        sparse_merges = ['context', 'features']
+        dense_merges = ['seq_len', 'label']
+        ignores = ['test', 'doc_ids', 'text_len', 'forloeb']
+
+        new_model = {}
+        
+        for key in sparse_merges:
+            new_shape = np.concatenate([[m1[key].dense_shape[0] + m2[key].dense_shape[0]], m1[key].dense_shape[1:]], axis=0)
+            new_indices = m1[key].indices
+            to_append = m2[key].indices
+            to_append[:,0] += m1[key].dense_shape[0]
+            new_indices = np.concatenate([new_indices, to_append], axis=0)
+            new_values = np.concatenate([m1[key].values, m2[key].values], axis=0)
+            
+            new_model[key] = tf.SparseTensorValue(new_indices, new_values, new_shape)
+        for key in dense_merges:
+            new_model[key] = np.concatenate([m1[key], m2[key]], axis=0)
+        for key in ignores:
+            new_model[key] = None
+
+        return new_model
+
+    def _merge_tensors_to_batch_size(self, tensors, batch_size):
+        new_tensors = []
+
+        current_tensor = tensors[0]
+        current_batch_size = 1
+        for t in tensors[1:]:
+            if current_batch_size == batch_size:
+                new_tensors.append(current_tensor)
+                current_batch_size = 1
+                current_tensor = t
+            else:
+                current_tensor = self._merge_models(current_tensor, t)
+                current_batch_size += 1
+
+        new_tensors.append(current_tensor)
+        return new_tensors
+
+    def has_next(self):
+        return self.records_read < self.record_count
+
+    def next_batch(self):
+        to_read = self.records_read
+        self.records_read += 1
+        return self.tensors[to_read]
+
+    def get_record_count(self):
+        return self.record_count
+    
+    def samples_read(self):
+        return self.records_read
+
+    def did_read_batch(self):
+        pass
+        
+if __name__ == '__main__':
+    fp = NpyFeatureParser(['./to_remove/test.npy'], 3)
+    

af3b40e67a5fb9fa8b89b70e3a36087a9fb1a21b
Frederik Hvilshøj <fhvilshoj@gmail.com>
2017-12-06 18:09:58 +0100

    Removed Winner takes all

--- config_selection/configurations.py
+++ config_selection/configurations.py
@@ -64,7 +64,7 @@ lstm_configurations = [
 
 max_pooling_configurations = [
     BaseConfiguration(RULE.WINNERS_TAKE_ALL),
-    BaseConfiguration(RULE.WINNER_TAKES_ALL),
+    # BaseConfiguration(RULE.WINNER_TAKES_ALL),
     # This second rule doesn't matter since implementation looks for WINNERS_TAKE_ALL or not
     BaseConfiguration(RULE.FLAT),
 ]
